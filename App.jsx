78911d9d182c4a13e51a0d06d44d3de68e495fd1
jaykarn singh <55475521+JaykarnSingh@users.noreply.github.com>
2022-07-16 17:57:49 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import React,{useState} from "react";
+
+const App =()=>{
+const[count,IncDecCount]=useState(0);
+
+const Inc=()=>{
+  IncDecCount(count+1);
+};
+const Dec=()=>{
+  IncDecCount(count-1);
+};
+
+return(
+  <>
+    <h1>{count}</h1>
+    <button onClick={Inc}>Click Increment</button>
+    <button onClick={Dec}>Click Decrement</button>
+  </>
+)
+}
+export default App;
